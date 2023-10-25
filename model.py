ff92f98edbcefcd1d0e860a582a26782f54a76e5
frowenz <owenfahey@uchicago.edu>
2023-10-25 02:11:13 -0500

    should be ready for an overnight run

--- model.py
+++ model.py
@@ -259,6 +259,31 @@ class GPT(nn.Module):
                     sd[k].copy_(sd_hf[k])
 
         return model
+    
+    def configure_SGD_optimizers(self, weight_decay, learning_rate, _, device_type):  # Removed betas as it's not used by SGD
+        # start with all of the candidate parameters
+        param_dict = {pn: p for pn, p in self.named_parameters()}
+        # filter out those that do not require grad
+        param_dict = {pn: p for pn, p in param_dict.items() if p.requires_grad}
+        # create optim groups. Any parameters that is 2D will be weight decayed, otherwise no.
+        # i.e. all weight tensors in matmuls + embeddings decay, all biases and layernorms don't.
+        decay_params = [p for n, p in param_dict.items() if p.dim() >= 2]
+        nodecay_params = [p for n, p in param_dict.items() if p.dim() < 2]
+        optim_groups = [
+            {'params': decay_params, 'weight_decay': weight_decay},
+            {'params': nodecay_params, 'weight_decay': 0.0}
+        ]
+        num_decay_params = sum(p.numel() for p in decay_params)
+        num_nodecay_params = sum(p.numel() for p in nodecay_params)
+        print(f"num decayed parameter tensors: {len(decay_params)}, with {num_decay_params:,} parameters")
+        print(f"num non-decayed parameter tensors: {len(nodecay_params)}, with {num_nodecay_params:,} parameters")
+
+        # Use SGD optimizer
+        optimizer = torch.optim.SGD(optim_groups, lr=learning_rate, momentum=0.9)  # Added momentum as a commonly used parameter for SGD, you can adjust it
+        print(f"using SGD optimizer")
+
+        return optimizer
+
 
     def configure_optimizers(self, weight_decay, learning_rate, betas, device_type):
         # start with all of the candidate parameters
